d7b20154b8c179a3583f2abbd4843660c3e57759
unknown <dlord4242@gmail.com>
2022-06-13 17:52:26 +0900

    Refactored styles

--- components/Element/Button/index.tsx
+++ components/Element/Button/index.tsx
@@ -1,13 +1,44 @@
+import { useMemo } from 'react';
+
 import classes from './index.module.scss';
 
 interface ButtonProps {
-  onClick: () => void;
+  wide?: boolean;
+  success?: boolean;
+  invalid?: boolean;
+  disabled?: boolean;
+  onClick?: () => void;
   children?: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+const Button: React.FC<ButtonProps> = ({
+  wide,
+  success,
+  invalid,
+  disabled,
+  onClick,
+  children,
+}) => {
+  const buttonClasses = useMemo(() => {
+    const classArray = [classes.button];
+
+    if (wide) {
+      classArray.push(classes.wide);
+    }
+
+    if (success) {
+      classArray.push(classes.success);
+    }
+
+    if (invalid) {
+      classArray.push(classes.invalid);
+    }
+
+    return classArray.join(' ');
+  }, [wide, success, invalid]);
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={buttonClasses} disabled={disabled} onClick={onClick}>
       {children}
     </button>
   );
