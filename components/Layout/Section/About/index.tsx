38f0bfb1981ea15db2181f2a654ba4e0c0370246
unknown <dlord4242@gmail.com>
2022-06-19 23:51:15 +0900

    Fixed about section styles

--- components/Layout/Section/About/index.tsx
+++ components/Layout/Section/About/index.tsx
@@ -10,55 +10,63 @@ const About: React.FC = () => {
   return (
     <Section id="about">
       <div className={classes.container}>
-        <h1>Tempor irure officia</h1>
-        <p>
-          Aute anim velit incididunt duis consequat laborum mollit exercitation
-          nulla occaecat excepteur irure.
-        </p>
-        <p>Excepteur nulla est enim duis duis anim ut enim duis duis.</p>
-
-        <h1>Skills</h1>
-        <div className={classes.skills}>
-          <Icon label="typescript">
-            <SiTypescript />
-          </Icon>
-          <Icon label="javascript">
-            <SiJavascript />
-          </Icon>
-          <Icon label="react">
-            <FaReact />
-          </Icon>
-          <Icon label="redux">
-            <SiRedux />
-          </Icon>
-          <Icon label="nodejs">
-            <FaNodeJs />
-          </Icon>
-          <Icon label="mongodb">
-            <SiMongodb />
-          </Icon>
-          <Icon label="css">
-            <FaCss3Alt />
-          </Icon>
-          <Icon label="sass">
-            <FaSass />
-          </Icon>
+        <div className={classes.header}>
+          <h1>Tempor irure officia</h1>
+          <p>
+            Aute anim velit incididunt duis consequat laborum mollit
+            exercitation nulla occaecat excepteur irure.
+          </p>
+          <p>Excepteur nulla est enim duis duis anim ut enim duis duis.</p>
         </div>
 
-        <h1>Certification</h1>
-        <div className={classes.certification}>
-          <Script
-            type="text/javascript"
-            async
-            src="//cdn.credly.com/assets/utilities/embed.js"
-          />
-          <div
-            data-iframe-width="110"
-            data-iframe-height="150"
-            data-share-badge-id="9728ee39-7a02-460a-b168-66ae5c4f44c0"
-            data-share-badge-host="https://www.credly.com"
-          />
-          <h4>AWS Certified Developer - Associate</h4>
+        <div className={classes.main}>
+          <div>
+            <h1>Skills</h1>
+            <div className={classes.skills}>
+              <Icon label="typescript">
+                <SiTypescript />
+              </Icon>
+              <Icon label="javascript">
+                <SiJavascript />
+              </Icon>
+              <Icon label="react">
+                <FaReact />
+              </Icon>
+              <Icon label="redux">
+                <SiRedux />
+              </Icon>
+              <Icon label="nodejs">
+                <FaNodeJs />
+              </Icon>
+              <Icon label="mongodb">
+                <SiMongodb />
+              </Icon>
+              <Icon label="css">
+                <FaCss3Alt />
+              </Icon>
+              <Icon label="sass">
+                <FaSass />
+              </Icon>
+            </div>
+          </div>
+
+          <div>
+            <h1>Certification</h1>
+            <div className={classes.certification}>
+              <Script
+                type="text/javascript"
+                async
+                src="//cdn.credly.com/assets/utilities/embed.js"
+              />
+              <div
+                data-iframe-width="110"
+                data-iframe-height="150"
+                data-share-badge-id="9728ee39-7a02-460a-b168-66ae5c4f44c0"
+                data-share-badge-host="https://www.credly.com"
+              />
+              <h4>AWS Certified Developer - Associate</h4>
+            </div>
+          </div>
         </div>
       </div>
     </Section>
